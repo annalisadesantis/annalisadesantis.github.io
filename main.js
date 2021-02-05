6d5af07e6987b88034836295e5ed40bd2ee1b4a0
Annalisa <annalisadesantis770@gmail.com>
2021-02-05 21:41:49 +0100

    home

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
+$(document).ready(function() {
 
-// Hamburger-menu appare al click
-$(".fa-bars").click(function(){
-    $(".hamburger-menu").slideToggle("slow");
-});
+    // Hamburger-menu appare al click
+    $(".fa-bars").click(function(){
+        $(".hamburger-menu").slideToggle("slow");
+    });
 
-// Hamburger-menu scompare al click
-$(".close").click(function(){
-    $(".hamburger-menu").slideToggle();
-});
+    // Hamburger-menu scompare al click
+    $(".close").click(function(){
+        $(".hamburger-menu").slideToggle();
+    });
 
-$(".link").click(function(){
-    $(".hamburger-menu").slideToggle();
+    $(".link").click(function(){
+        $(".hamburger-menu").slideToggle();
+    });
 });
